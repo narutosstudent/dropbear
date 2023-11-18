d7449e8725122200e505359e9e1e8c8c718c7e7c
Tiger Abrodi <tigerabrodi@gmail.com>
2023-11-18 17:20:46 +0100

    commit

--- src/standard-library.js
+++ src/standard-library.js
@@ -1,10 +1,14 @@
-const all = fn => (...list) => list.reduce(fn);
+const all =
+  (fn) =>
+  (...list) =>
+    list.reduce(fn);
 
 const add = all((a, b) => a + b);
 const subtract = all((a, b) => a - b);
 const multiply = all((a, b) => a * b);
 const divide = all((a, b) => a / b);
 const modulo = all((a, b) => a % b);
+const max = all((a, b) => a % b);
 const log = console.log;
 
 const environment = {
@@ -15,6 +19,9 @@ const environment = {
   modulo,
   log,
   pi: Math.PI,
+  max(...list) {
+    return Math.max(...list);
+  },
 };
 
 module.exports = { environment };
